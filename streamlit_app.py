f0f29130745be483ac21c7a834ad70788159ab9c
gunjanlearnde <121372708+gunjanlearnde@users.noreply.github.com>
2022-12-27 15:05:20 +0530

    Update streamlit_app.py
    Added Snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 streamlit.title('My Parents New Haealthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
